aa270bfe0eb977161ecd89061c9a1a7b37a5d4ef
monji05 <monji4869@gmail.com>
2026-05-02 22:14:00 +0900

    refactor: 既存のデータをマージしたあとで上書きするようにしたことでコメントが不要に

--- cmd/root.go
+++ cmd/root.go
@@ -91,7 +91,6 @@ to quickly create a Cobra application.`,
     for index, record := range records {
       if today == record.Date {
 				isTodayFlg = true
-        // 同じ日付の既存要素を削除
         records[index].Contents = append(records[index].Contents, newContents...)
 				break
       }
